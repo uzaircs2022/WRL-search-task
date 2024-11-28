d52270ac4324e575b4351bf3ce3d4157c5cf2c10
uzaircs2022 <syeduzair222.sus@gmail.com>
2024-11-28 07:56:23 -0500

    Moved Sensitive info into ENV

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,14 +7,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAcIcR_C1qH9JLuOSM0-Qy4clbrjDqnWgw",
-  authDomain: "digital-world-logbook-dev.firebaseapp.com",
-  projectId: "digital-world-logbook-dev",
-  storageBucket: "digital-world-logbook-dev.appspot.com",
-  messagingSenderId: "610788060881",
-  appId: "1:610788060881:web:00c891f19c2ee69b889ff0",
-  measurementId: "G-BVM20TJDPH"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
